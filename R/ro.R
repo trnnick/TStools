678638517b3f8eedfe052174788334ce95a0bcbb
Ivan Svetunkov <ivan@svetunkov.ru>
2015-06-19 20:04:34 -0700

    Added constant in / out sample window and implemented call with several values

--- R/ro.R
+++ R/ro.R
@@ -1,9 +1,13 @@
-ro <- function(data,h=1,origins=1,call,value=NULL,all=FALSE){
+ro <- function(data,h=1,origins=1,call,value=NULL,
+               ci=FALSE,co=FALSE){
 # Function makes Rolling Origin for the data using the call
 #    Copyright (C) 2015  Yves Sagaert & Ivan Svetunkov
+  l.value <- length(value)
   if(!is.null(value)){
-    if(substring(value,1,1)!="$"){
-      value <- paste0("$",value)
+    for(i in 1:l.value){
+      if(substring(value[i],1,1)!="$"){
+        value[i] <- paste0("$",value[i])
+      }
     }
   }
 
@@ -11,6 +15,10 @@ ro <- function(data,h=1,origins=1,call,value=NULL,all=FALSE){
   if(!is.numeric(data) & !is.ts(data)){
     stop("The provided data is not a vector or ts object! Can't work with it!", call.=FALSE);
   }
+
+# Write down the start and frequency
+  data.start <- start(data)
+  data.freq <- frequency(data)
   
 # Write down the entire dataset y and the original horizon
   y <- data
@@ -20,23 +28,36 @@ ro <- function(data,h=1,origins=1,call,value=NULL,all=FALSE){
 
 # Make matrix to store the results origins in colnames,
 #   different rows are entire horizons forecasted
-  forecasts <- matrix(NA,nrow=h,ncol=origins)
-  colnames(forecasts) <- paste0("origin",c(1:origins))
-  rownames(forecasts) <- paste0("h",c(1:h))
-  actuals <- forecasts
+#  forecasts <- array(NA,c(h,origins,length(value)))
+#  dimnames(forecasts) <- list(paste0("h",c(1:h)),
+#                              paste0("origin",c(1:origins)),
+#                              value)
+  actuals <- matrix(NA,nrow=h,ncol=origins)
+  colnames(actuals) <- paste0("origin",c(1:origins))
+  rownames(actuals) <- paste0("h",c(1:h))
 
+#  forecasts <- actuals
+  forecasts <- list(NA)
   cat(paste0("Origins done:  "))
 
-  if(all==TRUE){
+  if(co==FALSE){
     for (i in 1:origins){
 # Adjust forecasting horizon to not exeed the sample size
       h <- min(hh,obs - (in.sample+i-1))
 # Make the in-sample
-      data <- y[1:(in.sample+i-1)]
+      if(ci==FALSE){
+        data <- ts(y[1:(in.sample-h+i-1)],start=data.start,frequency=data.freq)
+      }
+      else{
+        data <- ts(y[i:(in.sample-h+i-1)],start=data.start,frequency=data.freq)
+      }
 # Evaluate the call string and save to object o.m.
       o.m <- eval(parse(text=call))
 # Save the forecast and the corresponding actuals in matrices
-      forecasts[1:h,i] <- eval(parse(text=paste0("o.m",value)))
+#      forecasts[1:h,i] <- eval(parse(text=paste0("o.m",value)))
+      for(j in 1:l.value){
+        forecasts[[(i-1)*l.value+j]] <- eval(parse(text=paste0("o.m",value[j])))
+      }
       actuals[1:h,i] <- y[(in.sample+i):(in.sample+i+h-1)]
       cat(paste(rep("\b",nchar(i)),collapse=""))
       cat(i)
@@ -45,11 +66,19 @@ ro <- function(data,h=1,origins=1,call,value=NULL,all=FALSE){
   else{
     for (i in 1:origins){
 # Make the in-sample
-      data <- y[1:(in.sample+i-h)]
+      if(ci==FALSE){
+        data <- ts(y[1:(in.sample-h+i-1)],start=data.start,frequency=data.freq)
+      }
+      else{
+        data <- ts(y[i:(in.sample-h+i-1)],start=data.start,frequency=data.freq)
+      }
 # Evaluate the call string and save to object o.m.
       o.m <- eval(parse(text=call))
 # Save the forecast and the corresponding actuals in matrices
-      forecasts[,i] <- eval(parse(text=paste0("o.m",value)))
+#      forecasts[,i] <- eval(parse(text=paste0("o.m",value)))
+      for(j in 1:l.value){
+        forecasts[[(i-1)*l.value+j]] <- eval(parse(text=paste0("o.m",value[j])))
+      }
       actuals[,i] <- y[(in.sample+i-h+1):(in.sample+i)]
       cat(paste(rep("\b",nchar(i)),collapse=""))
       cat(i)
@@ -58,5 +87,23 @@ ro <- function(data,h=1,origins=1,call,value=NULL,all=FALSE){
 
   cat("\n")
 
-  return(list(forecasts=forecasts,actuals=actuals))
+  if(is.null(value)){
+    value <- "$output"
+  }
+  value <- substring(value,2,nchar(value))
+
+  returned.list <- list(actuals)
+
+# Create several matrices from the list
+  for(i in 1:l.value){
+    stuff <- matrix(NA,nrow=length(forecasts[[i]]),ncol=origins)
+    colnames(stuff) <- colnames(actuals)
+    for(j in 1:origins){
+      stuff[1:length(forecasts[[(j-1)*l.value+i]]),j] <- forecasts[[(j-1)*l.value+i]]
+    }
+    returned.list[[i+1]] <- stuff
+  }
+
+  names(returned.list) <- c("actuals",value)
+  return(returned.list)
 }
