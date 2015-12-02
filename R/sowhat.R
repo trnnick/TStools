da707da15d26e859c65cc1409b693dd395bb2b14
Ivan Svetunkov <ivan.svetunkov@gmail.com>
2015-12-02 13:58:56 +0000

    A crucial update in the core function of TStools
    Hope now the package runs smoother.

--- R/sowhat.R
+++ R/sowhat.R
@@ -1,7 +1,24 @@
 sowhat <- function(...){
 # Function returns ultimate answer to any question
     if(any(grepl("\\?",unlist(list(...))))){
-        message("42");
+        message("42\n");
     }
+
+    cat("oooo+++o+ooossssso+++ooo+oo+++\n")
+    cat("ooooo++oydNNNNmmdmdyoo+++++++/\n")
+    cat("o+++oshmNNmNNmmmmmmmdds+++++//\n")
+    cat("o+++sdmNmdhhyyyhmmmdddmo+/////\n")
+    cat("+++odddmh+/:--::odddddmdyo////\n")
+    cat("+/+dmmdms/:-----:odNmdNmmms+++\n")
+    cat("+/oNNmmNyso+::oo+//yhhhdmNy+++\n")
+    cat("+/smNNNmssss/:::----//+mmh////\n")
+    cat("+//odNMdo/oo/::-----:smNmho+//\n")
+    cat("+//++yNNy/+s+::::---:smNNNd+//\n")
+    cat("/+/+++mNNy+o+//-..---/smmmy///\n")
+    cat("/::/++hNNNdy+/-----...+mmmhs+/\n")
+    cat("/:://+oydmNdoo+/:-...+dddmNNmm\n")
+    cat("/+ohdmNNNNmNoys/....+ddmmmNNNN\n")
+# Website used (for future): http://www.text-image.com/convert/ascii.html
+
     message("So what?");
 }
