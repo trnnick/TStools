1baffa8f3a5b4c7752429649cf1ce5c860751a2e
config-i1 <ivan.svetunkov@gmail.com>
2016-01-17 11:55:37 +0000

    New function - graphmaker - for the universal linear graphs. Currently used by "es" and "sma".

--- R/sma.R
+++ R/sma.R
@@ -119,63 +119,9 @@ sma <- function(x, order=NULL, h=10, optimize=FALSE,
         print(paste0("SMAPE: ",errormeasures["SMAPE"]*100,"%"));
         print(paste0("MASE: ",errormeasures["MASE"]));
     }
-    
-    plot.range <- range(min(x[!is.na(x)],trn.sma[!is.na(trn.sma)],tst.frcst[!is.na(tst.frcst)]),
-                        max(x[!is.na(x)],trn.sma[!is.na(trn.sma)],tst.frcst[!is.na(tst.frcst)]))
-    def.par <- par(no.readonly = TRUE)
+# Build a graph
+    graphmaker(actuals=x,forecast=tst.frcst,fitted=trn.sma,legend=legend);
 
-    if(legend==TRUE){
-      layout(matrix(c(1,2),2,1),heights=c(0.86,0.14))
-    }
-    par(mar=c(3,3,2,1))
-    plot(x,type="l",xlim=range(time(x)[1],time(tst.frcst)[h]),
-         ylim=plot.range,xlab="", ylab="")
-    lines(trn.sma,col="purple",lwd=2,lty=2)
-    
-    if(h>1){
-      lines(tst.frcst,col="blue",lwd=2)
-
-      if(legend==TRUE){
-# Define where to position the legend
-#        if(mean(c(trn.sma[!is.na(trn.sma)],tst.frcst)[1:round(n.all/3,0)])<(plot.range[2]+plot.range[1])/2){
-        leg.place = "bottom"
-#        }
-#        else{
-#          leg.place = "bottom"
-#        }
-
-        par(cex=0.75,mar=rep(0.1,4),bty="n",xaxt="n",yaxt="n")
-        plot(0,0,col="white")
-        legend(x=leg.place,
-               legend=c("Series","Fitted values","Point forecast","Forecast origin"),
-               col=c("black","purple","blue","red"),
-               lwd=c(1,2,2,2),
-               lty=c(1,2,1,1),ncol=2)
-      }
-    }
-    else{
-      points(tst.frcst,col="blue",lwd=2,pch=4)
-      if(legend==TRUE){
-# Define where to position the legend
-#        if(mean(c(trn.sma[!is.na(trn.sma)],tst.frcst)[1:round(n.all/3,0)])<(plot.range[2]+plot.range[1])/2){
-#          leg.place = "topleft"
-#        }
-#        else{
-#          leg.place = "bottomleft"
-#        }
-        leg.place = "bottom"
-
-        par(cex=0.75,mar=rep(0.1,4),bty="n",xaxt="n",yaxt="n")
-        plot(0,0,col="white")
-        legend(x=leg.place,
-               legend=c("Series","Fitted values","Point forecast","Forecast origin"),
-               col=c("black","purple","blue","red"),
-               lwd=c(1,2,2,2),
-               lty=c(1,2,NA,1),
-               pch=c(NA,NA,4,NA),ncol=2)
-      }
-    }
-    par(def.par)
   }
 
   return(list(order = order, fitted = trn.sma, forecast = tst.frcst));
