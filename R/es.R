4a7dc6c23ca2dffde901949f05308c34977af7d0
trnnick <nikolaos@kourentzes.com>
2016-12-07 11:51:39 +0000

    removed es and sim.ets

--- R/es.R
+++
@@ -1,6 +0,0 @@
-es <- function(...){
-    warning("Unfortunately es() function is no longer available in \"TStools\". It is now part of \"smooth\" package. \nInstalling 'smooth' is easy - just type install.packages(\"smooth\") and you are done!",call.=FALSE);
-}
-sim.ets <- function(...){
-    warning("Unfortunately sim.ets() function is no longer available in \"TStools\". It is now part of \"smooth\" package. \nInstalling 'smooth' is easy - just type install.packages(\"smooth\") and you are done!",call.=FALSE);
-}
