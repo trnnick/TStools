59b24df1b3c12d284b84058487e68fbc64b7cb5f
trnnick <nikolaos@kourentzes.com>
2017-02-12 20:51:34 +0000

    MLP/ELM forecast plot now really plots when h==1

--- R/mlp.R
+++ R/mlp.R
@@ -352,6 +352,13 @@ plot.forecast.net <- function(x,...){
     ts.plot(x$x,x$all.mean,x$mean,
             col=c("black",rep("grey",reps),"blue"),lwd=c(1,rep(1,reps),2),
             main=paste("Forecasts from",toupper(method)))
+    # If h==1 then use markers
+    if (length(x$mean)==1){
+        points(rep(time(x$mean),reps+1),c(x$all.mean,x$mean),
+               pch=c(rep(1,reps),20),col=c(rep("grey",reps),"blue"))
+    }
+    
+    
 }
     
 mlp.thief <- function(y,h=NULL,...){
