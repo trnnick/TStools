64d14a390fff8aa55a63cfa4e6d5c8f6af8933c0
Ivan Svetunkov <ivan@svetunkov.ru>
2017-05-18 14:49:13 +0100

    Prediction intervals based on two sigmas (left and right)

---
+++ R/twoSigmas.R
@@ -0,0 +1,43 @@
+twoSigmas <- function(model, level){
+    if(length(class(model))==1){
+        if(class(model)!="smooth"){
+            stop("Sorry, but we need smooth object for this stuff. And your model is not it...");
+        }
+    }
+    else{
+        stop("Sorry, but we need smooth object for this stuff. And your model is not it...");
+    }
+    
+    errors <- model$errors;
+    h <- ncol(errors);
+    errors <- errors[-c(1:(h-1)),];
+    
+    if(level>1){
+        if(level>100){
+            warning("The specified level is meaningless. Swithing to 95%.");
+            level <- 95;
+        }
+        level <- level/100;
+    }
+    
+    lower <- rep(NA,h);
+    upper <- rep(NA,h);
+    
+    sigmaL <- rep(NA,h);
+    sigmaU <- rep(NA,h);
+    
+    for(i in 1:h){
+        sigmaL[i] <- mean((errors[errors[,i]<0,i])^2)
+        sigmaU[i] <- mean((errors[errors[,i]>=0,i])^2)
+        
+        lower[i] <- model$forecast[i] + sigmaL[i] * qnorm((1-0.95)/2);
+        upper[i] <- model$forecast[i] + sigmaU[i] * qnorm((1+0.95)/2);
+    }
+    
+    sigma <- rbind(sigmaL,sigmaU);
+    dimnames(sigma) <- list(c("Lower sigma","Upper sigma"),paste0("h",1:h));
+    
+    names(lower) <- paste0("h",1:h);
+    names(upper) <- paste0("h",1:h);
+    return(list(lower=lower,upper=upper,sigma=sigma));
+}
