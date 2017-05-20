fc71dbef8fec691a98aa1f2c5b8e9fbb05fd64ce
trnnick <nikolaos@kourentzes.com>
2017-05-20 12:36:03 +0100

    Added intervals spline

---
+++ R/intervals-spline.R
@@ -0,0 +1,50 @@
+intervals.spline <- function(model,level=0.95,type=c("multi","single"),quantiletype=7){
+  # Calculate empirical PIs 
+    
+  # Arguments checks
+  type <- match.arg(type,c("multi","single"))
+  
+  if (!any(class(model)=="smooth")){
+    stop("Argument model should be of class `smooth'");
+  }
+  
+  if (any(level>1) | length(level)>1){
+    stop("Level should be a positive scalar < 1.")
+  }
+  
+  # PI level
+  b <- (1-level)/2 + c(0,1)*level
+  
+  # Get error matrix from model
+  eh <- model$errors
+  h <- dim(eh)[2]       # Forecast horizon
+  eh <- eh[-(1:h),]
+  
+  # Calculate intervals
+  lower <- upper <- vector("numeric",h)
+  # Use only t+1 errors or up to t+h
+  if (type == "single"){
+    J <- 1
+  } else {
+    J <- h
+  }
+    
+  for (j in 1:J){
+    spl <- stats::spline(x=1:length(eh[,j]), y=sort(eh[,j]), method="hyman")
+    x <- as.numeric(quantile(spl$y,b,type=quantiletype))
+    lower[j] <- x[1]
+    upper[j] <- x[2]
+  }
+  
+  if (type == "single"){
+    upper[] <- upper[1]*sqrt(1:h)
+    lower[] <- lower[1]*sqrt(1:h)
+  }
+  
+  # Add to forecasts
+  lower <- model$forecast + lower
+  upper <- model$forecast + upper
+
+  return(list(lower=lower,upper=upper))
+  
+}
